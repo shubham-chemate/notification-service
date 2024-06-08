7070bf8f18c61efb380e86b43485edfba78e19ff
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-06-08 21:11:18 +0530

    send notification through post endpoint

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 )
+
+var localCache map[string][]string
+
+type Notification struct {
+	UserName string `json:"sendTo"`
+	Data     string `json:"data"`
+}
+
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
 
 func sendNotification(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -13,7 +24,21 @@ func sendNotification(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("%+v", r)
+	var newNotification Notification
+	if err := json.NewDecoder(r.Body).Decode(&newNotification); err != nil {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	userName := newNotification.UserName
+	data := newNotification.Data
+
+	log.Println("Received new notification to send")
+	log.Printf("UserName: %v", userName)
+	log.Printf("Data: %v", data)
+
+	localCache[userName] = append(localCache[userName], data)
+
 	w.Write([]byte("Got Notification"))
 }
 
@@ -23,19 +48,28 @@ func getNotifications(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/event-stream")
+	// w.Header().Set("Content-Type", "text/event-stream")
 	// w.Header().Set("Cache-Control", "no-cache")
 	// w.Header().Set("Connection", "keep-alive")
 
-	for i := 0; i < 10; i++ {
-		fmt.Fprintf(w, "data: %s\n\n", fmt.Sprintf("Event %d", i))
-		time.Sleep(2 * time.Second)
-		w.(http.Flusher).Flush()
+	// for _, v := range localCache["0101"] {
+	// 	log.Println(v)
+	// }
 
-	}
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Fprintf(w, "data: %s\n\n", fmt.Sprintf("Event %d", i))
+	// 	time.Sleep(2 * time.Second)
+	// 	w.(http.Flusher).Flush()
+
+	// }
+
+	w.Write([]byte("wait a while"))
 }
 
 func main() {
+	log.Println("Starting notifcation-service")
+	localCache = make(map[string][]string)
+
 	http.HandleFunc("/send-notification", sendNotification)
 	http.HandleFunc("/get-notifications", getNotifications)
 	err := http.ListenAndServe(":8000", nil)
