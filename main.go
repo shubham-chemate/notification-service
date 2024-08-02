b5fc8981f8646b06522d5c46b09f5a9dda741124
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-08-02 23:33:16 +0530

    file read and notification processing

--- main.go
+++ main.go
@@ -1,97 +1,76 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"log"
-	"net/http"
 	"os"
+	"time"
 )
 
-var localCache map[string][]string
-
-type Notification struct {
-	UserName string `json:"sendTo"`
-	Data     string `json:"data"`
+type Message struct {
+	SendFrom string `json:sendFrom`
+	SendTo   string `json:sendTo`
+	Text     string `json:text`
 }
 
-func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("500 Internal Server Error"))
+var readyToSendMsgs map[string][]Message
+
+func readMessages(ch chan<- Message) {
+	file, err := os.Open("./data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// optional: resize scanner capacity for lines over 64K
+	// const mxCap int = 70000
+	// buf := make([]byte, mxCap)
+	// scanner.Buffer(buf, mxCap)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		msg := Message{}
+		json.Unmarshal([]byte(s), &msg)
+		ch <- msg
+	}
+
+	close(ch)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func sendNotification(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Write([]byte("wrong http method type, expected:POST, got:" + r.Method))
-		return
+func processMessage(ch <-chan Message) {
+	for msg := range ch {
+		// log.Printf("Received Message For Processing: %s", msg)
+		log.Printf("Received Message For Processing..")
+		// Process the message
+		rcvMsgs, ok := readyToSendMsgs[msg.SendTo]
+		if !ok {
+			rcvMsgs = []Message{}
+		}
+		rcvMsgs = append(rcvMsgs, msg)
+		readyToSendMsgs[msg.SendTo] = rcvMsgs
 	}
-
-	var newNotification Notification
-	if err := json.NewDecoder(r.Body).Decode(&newNotification); err != nil {
-		InternalServerErrorHandler(w, r)
-		return
-	}
-
-	userName := newNotification.UserName
-	data := newNotification.Data
-
-	log.Println("Received new notification to send")
-	log.Printf("UserName: %v, Data: %v", userName, data)
-
-	localCache[userName] = append(localCache[userName], data)
-
-	w.Write([]byte("Got Notification"))
-}
-
-func getNotifications(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.Write([]byte("wrong http method type expected:POST, got:" + r.Method))
-		return
-	}
-
-	userName := r.URL.Path[len("/get-notifications/"):]
-	log.Printf("Requested notification for user: %v", userName)
-
-	notifications, ok := localCache[userName]
-	if !ok {
-		w.Write([]byte("Don't have notifications"))
-		return
-	}
-
-	log.Printf("sending notification to user: %+v", notifications[0])
-	w.Write([]byte(notifications[0]))
-
-	// w.Header().Set("Content-Type", "text/event-stream")
-	// w.Header().Set("Cache-Control", "no-cache")
-	// w.Header().Set("Connection", "keep-alive")
-
-	// for _, v := range localCache["0101"] {
-	// 	log.Println(v)
-	// }
-
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Fprintf(w, "data: %s\n\n", fmt.Sprintf("Event %d", i))
-	// 	time.Sleep(2 * time.Second)
-	// 	w.(http.Flusher).Flush()
-
-	// }
-
-	// w.Write([]byte("wait a while"))
 }
 
 func main() {
-	log.Println("Starting notifcation-service")
-	localCache = make(map[string][]string)
+	log.Println("HELLO :)")
 
-	http.HandleFunc("/send-notification", sendNotification)
-	http.HandleFunc("/get-notifications/", getNotifications)
+	ch := make(chan Message)
+	readyToSendMsgs = make(map[string][]Message)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+	go readMessages(ch)
 
-	err := http.ListenAndServe("0.0.0.0:"+port, nil)
-	if err != nil {
-		log.Println("Error Starting the Server", err)
+	go processMessage(ch)
+
+	time.Sleep(5 * time.Second)
+
+	for k, _ := range readyToSendMsgs {
+		log.Println(k)
 	}
 }
---
+++ server/rest-endpoint.go
@@ -0,0 +1,98 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+var localCache map[string][]string
+
+type Notification struct {
+	UserName string `json:"sendTo"`
+	Data     string `json:"data"`
+}
+
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
+
+func sendNotification(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Write([]byte("wrong http method type, expected:POST, got:" + r.Method))
+		return
+	}
+
+	var newNotification Notification
+	if err := json.NewDecoder(r.Body).Decode(&newNotification); err != nil {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	userName := newNotification.UserName
+	data := newNotification.Data
+
+	log.Println("Received new notification to send")
+	log.Printf("UserName: %v, Data: %v", userName, data)
+
+	localCache[userName] = append(localCache[userName], data)
+
+	w.Write([]byte("Got Notification"))
+}
+
+func getNotifications(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.Write([]byte("wrong http method type expected:POST, got:" + r.Method))
+		return
+	}
+
+	userName := r.URL.Path[len("/get-notifications/"):]
+	log.Printf("Requested notification for user: %v", userName)
+
+	notifications, ok := localCache[userName]
+	if !ok {
+		w.Write([]byte("Don't have notifications"))
+		return
+	}
+
+	log.Printf("sending notification to user: %+v", notifications[0])
+	w.Write([]byte(notifications[0]))
+
+	// w.Header().Set("Content-Type", "text/event-stream")
+	// w.Header().Set("Cache-Control", "no-cache")
+	// w.Header().Set("Connection", "keep-alive")
+
+	// for _, v := range localCache["0101"] {
+	// 	log.Println(v)
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Fprintf(w, "data: %s\n\n", fmt.Sprintf("Event %d", i))
+	// 	time.Sleep(2 * time.Second)
+	// 	w.(http.Flusher).Flush()
+
+	// }
+
+	// w.Write([]byte("wait a while"))
+}
+
+func main() {
+	log.Println("Starting notifcation-service")
+	localCache = make(map[string][]string)
+
+	http.HandleFunc("/send-notification", sendNotification)
+	http.HandleFunc("/get-notifications/", getNotifications)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	// err := http.ListenAndServe("0.0.0.0:"+port, nil)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Println("Error Starting the Server", err)
+	}
+}
